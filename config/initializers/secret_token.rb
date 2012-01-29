7f41b5d109fe1d75606306a7c215f5103e82efe6
Gabe Kessler <gabe@impdesigns.com>
2012-01-29 16:52:47 -0500

    initial activeadmin setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopVibe::Application.config.secret_token = '30985d06544da1d33269e30f11ae70d2ac53a9de5ffdb2ff3f5cdff791e9529c4f1afdaad78fea87eecd924509f37d3abf31817af70631095f3de4a424d3c224'
